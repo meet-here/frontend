78b08a49ab1104b8fafe34d77936e3e102185d96
Maximilian Brehm <maximilian.brehm@student.hpi.de>
2015-06-20 16:13:33 +0200

    Add initial setup for leaflet

--- js/highway.js
+++ js/highway.js
@@ -5,8 +5,8 @@ var wsuri = (document.location.protocol === 'http:' ? 'ws:' : 'wss:') + '//' + d
 
 /// the WAMP connection to the Router
 var connection = new autobahn.Connection({
-	url: wsuri,
-  realm: 'realm1'
+    url: wsuri,
+    realm: 'realm1'
 });
 
 var name;
@@ -14,45 +14,45 @@ var global_session;
 var global_details;
 
 function set_hello (name) {
-	global_session.call('de.meet_here.hello', [name]).then(
-		function (res) {
-			console.log('de.meet_here.hello', res);
+    global_session.call('de.meet_here.hello', [name]).then(
+        function (res) {
+            console.log('de.meet_here.hello', res);
 
-			$('#hello').html(res);
-		},
-		function (err) {
-			console.log('Error when calling de.meet_here.hello', err);
+            $('#hello').html(res);
+        },
+        function (err) {
+            console.log('Error when calling de.meet_here.hello', err);
 
-			$('#hello').html('Default');
-		}
-	);
+            $('#hello').html('Default');
+        }
+    );
 }
 
 function set_name (args) {
-	name = args[0];
-	set_hello(name);
+    name = args[0];
+    set_hello(name);
 }
 
 // fired when connection is established and session attached
 connection.onopen = function (session, details) {
-	global_session = session;
-	global_details = details;
+    global_session = session;
+    global_details = details;
 
-	session.register('de.meet_here.set_name', set_name).then(
-		onSuccess =	function (reg) {
-			console.log('Registered de.meet_here.set_name');
-		},
-		onError = function (err) {
-			console.log('Error when registering de.meet_here.set_name');
-		}
-	);
+    session.register('de.meet_here.set_name', set_name).then(
+        onSuccess = function (reg) {
+            console.log('Registered de.meet_here.set_name');
+        },
+        onError = function (err) {
+            console.log('Error when registering de.meet_here.set_name');
+        }
+    );
 
-	set_hello("Guenther");
+    set_hello("Guenther");
 };
 
 // fired when connection was lost (or could not be established)
 connection.onclose = function (reason, details) {
-	console.log('Connection lost: ' + reason);
+    console.log('Connection lost: ' + reason);
 }
 
 // now actually open the connection
---
+++ js/map.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+    var map = L.map('map');
+
+    map.setView([52.509663, 13.376481], 14);
+    
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+       maxZoom: 18,
+       id: 'magmabyte.9c6c65af',
+       accessToken: 'pk.eyJ1IjoibWFnbWFieXRlIiwiYSI6IjMzMWJhZDY0NWI3NDEyNTViNjQ2ZTY1OTBmOWI1NDgyIn0.t3rvfNbF7wSl-VHWPu7BSg'
+    }).addTo(map);
+
+    function insertPolygon(coordinateList) {
+        return L.polygon(coordinateList).addTo(map);
+    }
+
+    function onMapClick(e) {
+        var popup = L.popup();
+
+        popup
+         .setLatLng(e.latlng)
+         .setContent("You clicked the map at " + e.latlng.toString())
+         .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+
+    insertPolygon([[52.509663, 13.376481], [52.519663, 13.376481], [52.519663, 13.386481]]); 
+})
+
