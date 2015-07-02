38709d92cc8d09f24c85efa1138d8bb20f5a890c
sknebel <sknebel@users.noreply.github.com>
2015-07-02 21:23:36 +0200

    remove useless comments

--- apps/mapview/mapview.js
+++ apps/mapview/mapview.js
@@ -1,37 +1,3 @@
-/*(function() {
-    var app = angular.module('mapview', []);
-    app.controller('MapViewController', [function() {
-        var map = L.map('map');
-
-        map.setView([52.509663, 13.376481], 14);
-
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18,
-            id: 'magmabyte.9c6c65af',
-            accessToken: 'pk.eyJ1IjoibWFnbWFieXRlIiwiYSI6IjMzMWJhZDY0NWI3NDEyNTViNjQ2ZTY1OTBmOWI1NDgyIn0.t3rvfNbF7wSl-VHWPu7BSg'
-        }).addTo(map);
-
-        function insertPolygon(coordinateList) {
-            return L.polygon(coordinateList).addTo(map);
-        }
-
-        function onMapClick(e) {
-            var popup = L.popup();
-
-            popup
-                .setLatLng(e.latlng)
-                .setContent("You clicked the map at " + e.latlng.toString())
-                .openOn(map);
-        }
-
-        map.on('click', onMapClick);
-
-        insertPolygon([[52.509663, 13.376481], [52.519663, 13.376481], [52.519663, 13.386481]]);
-    }]);
-})();*/
-
-
 (function() {
     var app = angular.module('mapview', ["leaflet-directive"]);
     app.controller('MapViewController', [ '$scope', function($scope) {
@@ -55,24 +21,5 @@
                 }
             }
         });
-    /*angular.extend($scope, {
-        london: {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 8
-        },
-        defaults: {
-
-            tileLayer: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-            tileLayerOptions: {
-                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-                maxZoom: 18,
-                id: 'magmabyte.9c6c65af',
-                accessToken: 'pk.eyJ1IjoibWFnbWFieXRlIiwiYSI6IjMzMWJhZDY0NWI3NDEyNTViNjQ2ZTY1OTBmOWI1NDgyIn0.t3rvfNbF7wSl-VHWPu7BSg',
-                reuseTiles: true,
-            },
-            scrollWheelZoom: false
-        }
-    });*/
 }]);
 })();
