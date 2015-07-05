42c4893c1d2e7e2802d078c62de869cba5bb06fe
Christian Bartz <christianbartz@gmail.com>
2015-07-05 18:41:51 +0200

    decouple angular routes

--- js/frontend.js
+++ js/frontend.js
@@ -2,10 +2,6 @@
     var app = angular.module('main', ['ngRoute', 'mapview', 'communicator']);
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/map', {
-                templateUrl: 'apps/mapview/mapview.html',
-                controller: 'MapViewController'
-            })
             .otherwise({
                 redirectTo: '/map'
             });
